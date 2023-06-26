1c2ebd6eb5aa4eb28b1f014b120b64f1d4c2f303
EvgeniiaS27 <adm.pr.salionova@gmail.com>
2023-06-26 21:25:30 +0300

    Task 01

---
+++ src/js/img-api.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+export async function fetchImages(search, page) {
+  const API_KEY = '37793380-846502e5eebe78c5507a83779';
+  const URL = `https://pixabay.com/api?q=${search}&key=${API_KEY}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`;
+
+  try {
+    const response = await axios.get(URL);
+    const data = await response.data;
+
+    return data;
+  } catch (err) {
+    return console.log(err);
+  }
+}
